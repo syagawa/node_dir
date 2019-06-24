c6eec0d2229a1750ee245a932316f9e3ce7b9207
yagawa <yagawa@qzc.co.jp>
2019-06-24 12:44:05 +0900

    Change stream.js

--- stream.js
+++ stream.js
@@ -6,28 +6,22 @@ ws.write("2 line\n");
 ws.write("3 line\n");
 ws.end("4 line");
 
-const rs = fs.createReadStream("stream.txt", { encoding: "utf8"});
-rs.on("data", function(data){
-  console.log(data);
-});
-rs.on("end", function(data){
-  console.log(data);
-});
+// const rs = fs.createReadStream("stream.txt", { encoding: "utf8"});
+// rs.on("data", function(data){
+//   console.log(data);
+// });
+// rs.on("end", function(data){
+//   console.log(data);
+// });
 
 
 const rs1 = fs.createReadStream("stream.txt", { encoding: "utf8"});
-const ws1 = fs.createWriteStream("stream_copy.txt", { encoding: "utf8" });
-rs1.pipe(ws1);
-ws1.write("aaaa");
-rs1.pipe(ws1);
-rs1.pipe(ws1);
+const ws1 = fs.createWriteStream("stream.txt", { encoding: "utf8" });
+const ws2 = fs.createWriteStream("stream_copy.txt", { encoding: "utf8" });
 
-// ws1.write("1 item\n");
-// ws1.write("2 item\n");
-// ws1.write("3 item\n");
-// rs1.pipe(ws1);
-// ws1.end();
-// rs1.on("end", function(data){
-//   console.log(data);
-// });
+rs1.on("data", function(d){
+  console.log("d", d);
+  // rs1.pipe(ws2);
+  ws2.write(d);
+});
 
